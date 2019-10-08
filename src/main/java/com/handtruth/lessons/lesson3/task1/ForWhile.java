1ec9e7beca8b2bbb33d7e271592bd4e6e05a7651
BigTicket <f-l-2010.finaev@yandex.ru>
2019-10-09 00:19:48 +0300

    lessons 3

--- src/main/java/com/handtruth/lessons/lesson3/task1/ForWhile.java
+++ src/main/java/com/handtruth/lessons/lesson3/task1/ForWhile.java
@@ -7,9 +7,13 @@ class ForWhile {
      * <p>
      * Вычислить факториал
      */
-    int factorial(int n) {
-        // TODO your code
-        return 0;
+    long factorial(int n) {
+        long k = 1;
+        while (n > 1){
+           k *= n;
+           n--;
+        }
+        return k;
     }
 
     /**
@@ -18,8 +22,15 @@ class ForWhile {
      * Найти количество цифр в заданном числе
      */
     int counter(int n) {
-        // TODO your code
-        return 0;
+        int k = 0;
+        if (n == 0){
+            return 1;
+        }
+        while (n != 0) {
+            k++;
+            n = n / 10;
+        }
+        return k;
     }
 
     /**
@@ -30,8 +41,20 @@ class ForWhile {
      * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
      */
     boolean isCoPrime(int a, int b) {
-        // TODO your code
-        return false;
+        while (a != b){
+            if (a > b){
+                a -= b;
+            }
+            else{
+                b -= a;
+            }
+        }
+        if (a == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 
@@ -43,8 +66,12 @@ class ForWhile {
      * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
      */
     double sin(double x, double exp) {
-        // TODO your code
-        return 0;
+        double sin = 0;
+        while (exp != 1){
+            sin += x + Math.pow(x,exp);
+            exp -= 1;
+        }
+        return sin;
     }
 
     /**
@@ -55,8 +82,16 @@ class ForWhile {
      * Например, 2-я цифра равна 4, 7-я 5, 12-я 6.
      */
     int squareSequenceDigit(int n) {
-        // TODO your code
-        return 0;
+        int m = 1;
+        String k = "";
+       while (m != n){
+           k += m*m;
+           m += 1;
+       }
+       k = k.substring(n-1,n);
+       int index = Integer.parseInt(k);
+       return index;
     }
+
 }
 
