cd441694ad7184944102327c5e3360532f4861f3
BigTicket <f-l-2010.finaev@yandex.ru>
2019-10-21 01:52:12 +0300

    lessons 1

--- src/main/java/com/handtruth/lessons/lesson5/task1/Parse.java
+++ src/main/java/com/handtruth/lessons/lesson5/task1/Parse.java
@@ -13,7 +13,13 @@ class Parse {
      * Для конкатинации строк использовать метод concat()
      */
     String toDigitStr(int n) {
-
+        if (n < 10) {
+            String one = "0";
+            String two = Integer.toString(n);
+            return one.concat(two);
+        } else {
+            return Integer.toString(n);
+        }
     }
 
     /**
@@ -24,6 +30,17 @@ class Parse {
      * Для конкатинации строк использовать StringBuilder
      */
     String timeSecondsToStr(int seconds) {
+        StringBuilder sb = new StringBuilder(8);
+        sb.append(seconds / 3600).append(":");
+        if ((seconds / 60) == 0 || (seconds % 60) == 0) {
+            sb.append((seconds / 60) % 60).append("0:0");
+            sb.append(seconds % 60);
+            return sb.toString();
+        }
+
+        sb.append((seconds / 60) % 60).append(":");
+        sb.append(seconds % 60);
+        return sb.toString();
 
     }
 
@@ -34,7 +51,8 @@ class Parse {
      * (пример двоичного числа: 100101)
      */
     boolean isBin(String n) {
-
+        String d = "[0-1]+"; // [0-1]+ (+) - один или несколько раз
+        return n.matches(d);
     }
 
     /**
@@ -44,7 +62,12 @@ class Parse {
      * Не использовать стандартную функцию substring()
      */
     String substring(String str, int n, int m) {
-
+        StringBuilder sb = new StringBuilder();
+        while (n != m) {
+            sb.append(str.charAt(n));
+            n++;
+        }
+        return sb.toString();
     }
 
     /**
@@ -55,5 +78,24 @@ class Parse {
      * Вернуть результат операции.
      */
     int mathOperation(String example) {
+        int sum = 0;
+        String example2 = example.replace(" ", "");
+        boolean Minus = example2.contains("-");
+        boolean Plus = example2.contains("+");
+        boolean Mult = example2.contains("*");
+
+        if (Minus) {
+            String[] Razbiv = example2.split("-");
+            sum = Integer.parseInt(Razbiv[0]) - Integer.parseInt(Razbiv[1]);
+        }
+        if (Plus) {
+            String[] Razbiv = example2.split(Pattern.quote("+"));
+            sum = Integer.parseInt(Razbiv[0]) + Integer.parseInt(Razbiv[1]);
+        }
+        if (Mult) {
+            String[] Razbiv = example2.split(Pattern.quote("*"));
+            sum = Integer.parseInt(Razbiv[0]) * Integer.parseInt(Razbiv[1]);
+        }
+        return sum;
     }
 }
